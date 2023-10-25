1d8b92af5e207811117bd2a85396bc76f49d5cc8
P S Shrijith <shrijithps89@gmail.com>
2023-10-25 23:02:26 +0530

    Update bandnamegenerator.py

--- bandnamegenerator.py
+++ bandnamegenerator.py
@@ -1,4 +1,1 @@
-print("Welcome to band name generator")
-city = input("Enter the name of your city\n")
-pet = input("Enter the name of your pet\n")
-print("Your band name is {} {}".format(city, pet))
+
