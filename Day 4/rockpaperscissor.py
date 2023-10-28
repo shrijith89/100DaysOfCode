fc9ca890413aa2cff8812c8453bcdb388e90316f
P S Shrijith <shrijithps89@gmail.com>
2023-10-28 10:08:56 +0530

    rockpaperscissor game using randomization

---
+++ Day 4/rockpaperscissor.py
@@ -0,0 +1,47 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+user_choice = int(input("Enter 0,1,2"))
+game_images = [rock, paper, scissors]
+computer_choice = random.randint(0, 2)
+
+print("Computer chose: ")
+print(game_images[computer_choice])
+print("User chose: ")
+print(game_images[user_choice])
+
+if ((user_choice == 2 and computer_choice == 1) or \
+        (user_choice == 1 and computer_choice == 0) or (user_choice == 0 and computer_choice == 2)):
+    print("You Win")
+elif user_choice == computer_choice:
+    print("It's a draw")
+elif user_choice >= 3 or user_choice < 0:
+    print("Invalid number")
+else:
+    print("You lose!")
