8b8262af80211e8ece3e755160bbd252a3e175a5
P S Shrijith <shrijithps89@gmail.com>
2023-11-02 23:35:45 +0530

    updated the code

--- Day 7/targetbehaviour.py
+++ Day 7/targetbehaviour.py
@@ -3,15 +3,15 @@ import random
 word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-guess_letter = input("Guess a letter: ").lower()
 
 display = []
 
 for letter in range(len(chosen_word)):
     display += '_'
 
-for letter in range(len(chosen_word)):
-    if chosen_word[letter] == guess_letter:
-        display[letter] = guess_letter
-
-print(display)
+while '_' in display:
+    guess_letter = input("Guess a letter: ").lower()
+    for letter in range(len(chosen_word)):
+        if chosen_word[letter] == guess_letter:
+            display[letter] = guess_letter
+    print(display)
