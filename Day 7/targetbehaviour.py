5b5ef5406d726efef1c8404824567ec1958704d0
P S Shrijith <shrijithps89@gmail.com>
2023-11-02 23:17:20 +0530

    updatedcode

--- Day 7/targetbehaviour.py
+++ Day 7/targetbehaviour.py
@@ -5,8 +5,13 @@ word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 guess_letter = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
-    if letter == guess_letter:
-        print("Right")
-    else:
-        print("Wrong")
+display = []
+
+for letter in range(len(chosen_word)):
+    display += '_'
+
+for letter in range(len(chosen_word)):
+    if chosen_word[letter] == guess_letter:
+        display[letter] = guess_letter
+
+print(display)
