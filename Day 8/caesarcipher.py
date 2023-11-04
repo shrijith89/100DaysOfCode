b70acf84ebc0b5cfd3f8ea8de83f5f1fad9e8be6
P S Shrijith <shrijithps89@gmail.com>
2023-11-04 18:47:13 +0530

    caesarcipher

---
+++ Day 8/caesarcipher.py
@@ -0,0 +1,19 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+alphabet_list = list(text)
+
+def encrypt(direction, text, shift):
+    encrypted_message = ""
+    for i in range(0, len(alphabet_list)):
+        if alphabet_list[i] in alphabet:
+            encrypted_message += alphabet[alphabet.index(alphabet_list[i]) + shift]
+    print("The encrypted message is {}".format(encrypted_message))
+
+encrypt(direction=direction, text=text, shift=shift)
