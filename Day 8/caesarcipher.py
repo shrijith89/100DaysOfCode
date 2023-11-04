e7e74f858d8c3a116eb549665e1fc698d68f5e13
P S Shrijith <shrijithps89@gmail.com>
2023-11-04 21:19:24 +0530

    caesor_cipher

--- Day 8/caesarcipher.py
+++ Day 8/caesarcipher.py
@@ -10,25 +10,21 @@ shift = int(input("Type the shift number:\n"))
 alphabet_list = list(text)
 
 
-def encode(direction=direction, text=text, shift=shift):
-    encrypted_message = ""
-    for i in range(0, len(alphabet_list)):
-        if alphabet_list[i] in alphabet:
-            encrypted_message += alphabet[alphabet.index(alphabet_list[i]) + shift]
-    print("The encrypted message is {}".format(encrypted_message))
+def caesar_cipher(direction_side, text, shift_amount):
+    if direction_side == "encode":
+        encrypted_message = ""
+        for i in range(0, len(alphabet_list)):
+            if alphabet_list[i] in alphabet:
+                encrypted_message += alphabet[alphabet.index(alphabet_list[i]) + shift_amount]
+        print("The encrypted message is {}".format(encrypted_message))
+    elif direction_side == "decode":
+        decrypted_message = ""
+        for i in range(0, len(alphabet_list)):
+            if alphabet_list[i] in alphabet:
+                decrypted_message += alphabet[alphabet.index(alphabet_list[i]) - shift_amount]
+        print("The decrypted message is {}".format(decrypted_message))
+    else:
+        print("Invalid direction!")
 
 
-def decode(direction=direction, text=text, shift=shift):
-    decrypted_message = ""
-    for i in range(0, len(alphabet_list)):
-        if alphabet_list[i] in alphabet:
-            decrypted_message += alphabet[alphabet.index(alphabet_list[i]) - shift]
-    print("The decrypted message is {}".format(decrypted_message))
-
-
-if direction == "encode":
-    encode(direction, text, shift)
-elif direction == "decode":
-    decode(direction, text, shift)
-else:
-    print("Invalid direction!")
+caesar_cipher(direction_side=direction, text=alphabet_list, shift_amount=shift)
