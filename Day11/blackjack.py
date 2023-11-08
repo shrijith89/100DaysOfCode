76e467c11cb8ab4a03d7ddf0da88a6ff4d157b11
P S Shrijith <shrijithps89@gmail.com>
2023-11-08 08:08:56 +0530

    blackjack

--- Day11/blackjack.py
+++ Day11/blackjack.py
@@ -11,6 +11,7 @@ def calculate_sum(cards):
         sumofcards += i
     return sumofcards
 
+
 def assign_cards(cardcollection):
     cardcollection = []
     for i in range(2):
@@ -20,14 +21,22 @@ def assign_cards(cardcollection):
 
 user_cards = assign_cards(deal_card())
 computer_cards = assign_cards(deal_card())
-
+need_card = False
 
 def winner():
+    updated_sum = 0
     if calculate_sum(user_cards) == 21:
         print("User Won.!")
     elif calculate_sum(computer_cards) == 21:
         print("User Lost! Computer Wins!")
-
+    else:
+        need_card = input("Do you want to select any other card, Type 'y' or 'n' ")
+        if need_card == 'y':
+            user_cards.append(assign_cards((deal_card())))
+            for i in range(len(user_cards)):
+                if user_cards[i] == 11:
+                    user_cards[i] = 1
+                updated_sum += user_cards[i]
 winner()
 
 
