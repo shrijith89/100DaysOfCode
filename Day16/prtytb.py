ae77fb8dd96d3b4762918e3d3bfe9052bc8eb62a
P S Shrijith <shrijithps89@gmail.com>
2023-11-16 07:50:47 +0530

    pretty_table

--- Day16/prtytb.py
+++ Day16/prtytb.py
@@ -1,9 +1,11 @@
 from prettytable import PrettyTable
+from prettytable.colortable import ColorTable, Themes
 
 x = PrettyTable()
 
 x.add_column("Places", ["Coorg", "Ooty"])
 x.add_column("State", ["Karnataka", "Tamilnadu"])
 x.align = 'c'
+#x = ColorTable(theme=Themes.OCEAN)
 
 print(x)
