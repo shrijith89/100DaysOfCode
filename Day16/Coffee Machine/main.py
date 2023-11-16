a440eda341567897d5c68819fce517d2758f5957
P S Shrijith <shrijithps89@gmail.com>
2023-11-16 23:27:07 +0530

    coffee+machine

--- Day16/Coffee Machine/main.py
+++ Day16/Coffee Machine/main.py
@@ -15,7 +15,10 @@ while is_on:
         coffee.report()
     else:
         drink = menu.find_drink(choice)
-        if coffee.is_resource_sufficient(choice):
+
+        if coffee.is_resource_sufficient(drink):
             money.process_coins()
+            money.make_payment(drink.cost)
+
 
 
