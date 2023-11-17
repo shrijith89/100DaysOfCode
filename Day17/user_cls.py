a37cba8d8081dac1e41cfa2955afa69b04caf788
P S Shrijith <shrijithps89@gmail.com>
2023-11-18 01:27:50 +0530

    classes_python

--- Day17/user_cls.py
+++ Day17/user_cls.py
@@ -1,12 +1,15 @@
 class User:
+    i = 0
 
     def __init__(self, name, id):
         self.name = name
         self.id = id
 
     def method(self):
-        print(self.name)
+        print(self.i)
 
 
 user1 = User("Test", 123)
 user2 = User("Test1", 456)
+user1.i = 20
+user1.method()
