017f9f703e62d0ef7fe1a70e9ddbed99e39448e7
P S Shrijith <shrijithps89@gmail.com>
2023-11-18 01:47:02 +0530

    classes_python

--- Day17/user_cls.py
+++ Day17/user_cls.py
@@ -4,12 +4,18 @@ class User:
     def __init__(self, name, id):
         self.name = name
         self.id = id
+        self.following = 0
+        self.followers = 0
 
-    def method(self):
-        print(self.i)
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user1 = User("Test", 123)
 user2 = User("Test1", 456)
-user1.i = 20
-user1.method()
+user2.follow(user1)
+print(user1.following)
+print(user1.followers)
+print(user2.following)
+print(user2.followers)
