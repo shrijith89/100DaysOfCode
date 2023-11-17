184fd26fa041fd639c9c8494761182ead5f708d6
P S Shrijith <shrijithps89@gmail.com>
2023-11-17 06:56:14 +0530

    coffee_machine

--- Day16/Coffee Machine/main.py
+++ Day16/Coffee Machine/main.py
@@ -2,8 +2,8 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-coffee = CoffeeMaker()
-money = MoneyMachine()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 menu = Menu()
 is_on = True
 
@@ -12,8 +12,8 @@ while is_on:
     if choice == "off":
         is_on = False
     elif choice == "report":
-        coffee.report()
+        coffee_maker.report()
     else:
         drink = menu.find_drink(choice)
-        if coffee.is_resource_sufficient(drink) and money.make_payment(drink.cost):
-            coffee.make_coffee(drink)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
