16dd901a1c4286eecafab4cd55336786d33d95c9
P S Shrijith <shrijithps89@gmail.com>
2023-11-18 22:05:30 +0530

    quiz_game

--- Day17/Quiz_Game/quiz_brain.py
+++ Day17/Quiz_Game/quiz_brain.py
@@ -1,5 +1,4 @@
 class QuizBrain:
-    def __init__(self, question_no, question_list):
+    def __init__(self, question_list):
         self.question_no = 0
         self.question_list = question_list
-
