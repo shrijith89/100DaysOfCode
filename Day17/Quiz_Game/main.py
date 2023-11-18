d07e2d156ba8c2ae873b0431c0af363bd1ed112c
P S Shrijith <shrijithps89@gmail.com>
2023-11-18 22:25:05 +0530

    quiz_game

--- Day17/Quiz_Game/main.py
+++ Day17/Quiz_Game/main.py
@@ -1,8 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for data in question_data:
     question = Question(data["text"], data["answer"])
     question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
--- Day17/Quiz_Game/quiz_brain.py
+++ Day17/Quiz_Game/quiz_brain.py
@@ -2,3 +2,7 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_no = 0
         self.question_list = question_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_no]
+        input("Q.{} {} (True or False)".format(self.question_no+1, current_question.text))
