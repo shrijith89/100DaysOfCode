73ff34d85c2581fa56a00a3553bd30fa92ebda95
P S Shrijith <shrijithps89@gmail.com>
2023-11-18 17:52:51 +0530

    quiz_game

--- Day17/Quiz_Game/main.py
+++ Day17/Quiz_Game/main.py
@@ -6,3 +6,9 @@ question_bank = []
 for data in question_data:
     question = Question(data["text"], data["answer"])
     question_bank.append(question)
+
+
+class QuizBrain:
+
+    def __init__(self, question_no):
+        self.question_no = question_no
