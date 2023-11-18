96067ae806524f0e0496ed87392396e87af16761
P S Shrijith <shrijithps89@gmail.com>
2023-11-18 17:27:05 +0530

    quiz_game

---
+++ Day17/Quiz_Game/main.py
@@ -0,0 +1,11 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for data in question_data:
+    question = Question(data.get("text"), data.get("answer"))
+    question_bank.append(question)
+
+for i in question_bank:
+    print(i)
