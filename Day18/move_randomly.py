0d90e6b9d17b8c9b22561c6101a9a024debfb47b
P S Shrijith <shrijithps89@gmail.com>
2023-11-21 20:02:15 +0530

    move_randomly

--- Day18/move_randomly.py
+++ Day18/move_randomly.py
@@ -4,11 +4,21 @@ import random
 colors = ['blue', 'yellow', 'red', 'black', 'brown']
 t.pensize(10)
 t.speed("fastest")
+screen = t.Screen()
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color_combo = (r, g, b)
+    return color_combo
 
 
 def move_random():
+    t.colormode(255)
     for _ in range(100):
-        t.pencolor(random.choice(colors))
+        t.pencolor(random_color())
         t.forward(25)
         t.setheading(random.choice(range(0, 360, 90)))
 
