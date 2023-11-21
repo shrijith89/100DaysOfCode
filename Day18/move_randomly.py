7929e7c03c56d431894beba0b74bf8c16179aac2
P S Shrijith <shrijithps89@gmail.com>
2023-11-21 09:36:43 +0530

    random_walk

--- Day18/move_randomly.py
+++ Day18/move_randomly.py
@@ -2,12 +2,13 @@ import turtle as t
 import random
 
 colors = ['blue', 'yellow', 'red', 'black', 'brown']
+t.pensize(10)
+t.speed("fastest")
 
 
 def move_random():
     for _ in range(100):
         t.pencolor(random.choice(colors))
-        t.pensize(10)
         t.forward(25)
         t.setheading(random.choice(range(0, 360, 90)))
 
