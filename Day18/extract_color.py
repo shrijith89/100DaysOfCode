8701aa6c8955b4c281612be8c6d1441b514abfa8
P S Shrijith <shrijithps89@gmail.com>
2023-11-22 17:02:32 +0530

    extract_color

--- Day18/extract_color.py
+++ Day18/extract_color.py
@@ -1,4 +1,5 @@
 import colorgram
+import turtle as t
 
 colors = colorgram.extract('spot_painting.jpg', 25)
 color_list = []
@@ -6,4 +7,17 @@ color_list = []
 for i in range(len(colors)):
     color_list.append((colors[i].rgb.r, colors[i].rgb.g, colors[i].rgb.b))
 
-print(color_list)
+
+def draw_dots():
+    t.colormode(255)
+    for i in range(9):
+        t.setx(0)
+        t.sety(t.ycor()+40)
+        for j in range(len(color_list)-5):
+            t.pencolor(color_list[j])
+            t.dot(20)
+            t.penup()
+            t.forward(30)
+
+
+draw_dots()
