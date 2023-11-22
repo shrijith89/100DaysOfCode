777f6855dac1d92da6b67c1205b3a8839c13b848
P S Shrijith <shrijithps89@gmail.com>
2023-11-22 07:02:59 +0530

    draw_circle

--- Day18/draw_circle.py
+++ Day18/draw_circle.py
@@ -2,7 +2,10 @@ import turtle as t
 
 t.speed("fastest")
 
-for _ in range(100):
+for _ in range(200):
     t.circle(100)
-    t.setheading(2)
+    t.left(2)
 
+for _ in range(200):
+    t.circle(100)
+    t.setheading(t.heading() + 2)
