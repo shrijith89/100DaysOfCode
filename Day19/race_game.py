f580ad9d0c4cbe47068074c19290864fa9c27d9d
P S Shrijith <shrijithps89@gmail.com>
2023-11-23 07:32:23 +0530

    race_game

--- Day19/race_game.py
+++ Day19/race_game.py
@@ -1,12 +1,21 @@
 import turtle
 from turtle import Turtle, Screen
 
-t = Turtle()
+
 screen = Screen()
 screen.setup(width=600, height=600)
 #user_bet = screen.textinput(title="Choose Color", prompt="Which turtle is going to win the race, Choose your color")
+colors = ['red', 'yellow', 'blue', 'green', 'purple', 'orange']
 
-t.hideturtle()
-turtle.penup()
-turtle.goto(-280, -250)
+ycor = -200
+xcor = -260
+
+for i in range(6):
+    t = Turtle(shape='turtle')
+    t.penup()
+    t.goto(xcor, ycor)
+    ycor += 40
+
+
+
 screen.exitonclick()
