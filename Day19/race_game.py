7f7b8d61ff01eb7638468aa2ed7fb273b2694046
P S Shrijith <shrijithps89@gmail.com>
2023-11-23 08:05:48 +0530

    race_game

--- Day19/race_game.py
+++ Day19/race_game.py
@@ -1,20 +1,35 @@
+import random
 import turtle
 from turtle import Turtle, Screen
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
-#user_bet = screen.textinput(title="Choose Color", prompt="Which turtle is going to win the race, Choose your color")
+user_bet = screen.textinput(title="Choose Color", prompt="Which turtle is going to win the race, Choose your color")
 colors = ['red', 'yellow', 'blue', 'green', 'purple', 'orange']
-
+all_turtles = []
+race_is_on = False
+winner = ""
 ycor = -200
 xcor = -260
 
 for i in range(6):
-    t = Turtle(shape='turtle')
-    t.penup()
-    t.color(colors[i])
-    t.goto(xcor, ycor)
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.penup()
+    new_turtle.color(colors[i])
+    new_turtle.goto(xcor, ycor)
     ycor += 40
+    all_turtles.append(new_turtle)
 
-screen.exitonclick()
+if user_bet:
+    race_is_on = True
+
+while race_is_on:
+    for trtle in all_turtles:
+        trtle.forward(random.randint(1,10))
+        if trtle.xcor() >= 260:
+            winner = trtle.color()[0]
+            race_is_on = False
+
+
+
