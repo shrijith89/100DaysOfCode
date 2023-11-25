2840d356e700f87219950ecdb927b79fe123ad1f
P S Shrijith <shrijithps89@gmail.com>
2023-11-25 22:01:25 +0530

    snake_game

--- Day20/Snake Game/food.py
+++ Day20/Snake Game/food.py
@@ -10,6 +10,7 @@ class Food(Turtle):
         self.penup()
         self.color('blue')
         self.speed('fastest')
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
