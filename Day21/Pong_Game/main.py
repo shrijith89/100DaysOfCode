9d41762d074c0c4f2c38b7c3d1604f8c002e7511
P S Shrijith <shrijithps89@gmail.com>
2023-11-30 21:38:05 +0530

    pong_game

--- Day21/Pong_Game/main.py
+++ Day21/Pong_Game/main.py
@@ -1,22 +1,24 @@
-import time
 import turtle
 from turtle import Turtle, Screen
 from paddle import Paddle
 
+MOVE_PADDLE = 40
+is_on = True
+
 screen = Screen()
-screen.bgcolor('white')
+screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
+
 paddle = Paddle()
-is_on = True
 screen.tracer(0)
 screen.listen()
 
 turtle.onkey(paddle.move_up, "Up")
 turtle.onkey(paddle.move_down, "Down")
 
+
 while is_on:
     screen.update()
-    time.sleep(0.1)
 
 screen.exitonclick()
--- Day21/Pong_Game/paddle.py
+++ Day21/Pong_Game/paddle.py
@@ -1,41 +1,21 @@
 from turtle import Turtle
 
-MOVE_SEGMENT = 40
-positions = [(350, 20), (350, 40), (350, 60), (350, 80)]
-
-turtle = Turtle()
+MOVE_PADDLE = 40
 
 
 class Paddle:
     def __init__(self):
-        self.segments = []
-        self.create_paddle()
-
-    def create_paddle(self):
-        for position in positions:
-            self.add_segment(position)
-
-    def add_segment(self, position):
-        new_turtle = Turtle(shape='square')
-        new_turtle.penup()
-        new_turtle.color('black')
-        new_turtle.goto(position)
-        self.segments.append(new_turtle)
+        self.paddle = Turtle()
+        self.paddle.shape("square")
+        self.paddle.color('white')
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+        self.paddle.penup()
+        self.paddle.goto(350, 0)
 
     def move_up(self):
-        self.segments[3].setheading(90)
-        self.segments[3].forward(MOVE_SEGMENT)
-        for turtle1 in range(len(self.segments) - 2, -1, -1):
-            new_xcor = self.segments[turtle1].xcor()
-            new_ycor = self.segments[turtle1].ycor() + MOVE_SEGMENT
-            self.segments[turtle1].goto(new_xcor, new_ycor)
+        new_y = self.paddle.ycor() + MOVE_PADDLE
+        self.paddle.goto(self.paddle.xcor(), new_y)
 
     def move_down(self):
-        self.segments[0].setheading(270)
-        self.segments[0].forward(MOVE_SEGMENT)
-        for turtle1 in range(len(self.segments)-1, 0, -1):
-            new_xcor = self.segments[turtle1].xcor()
-            new_ycor = self.segments[turtle1].ycor() - MOVE_SEGMENT
-            self.segments[turtle1].goto(new_xcor, new_ycor)
-
-
+        new_y = self.paddle.ycor() - MOVE_PADDLE
+        self.paddle.goto(self.paddle.xcor(), new_y)
