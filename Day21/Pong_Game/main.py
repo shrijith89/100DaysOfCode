cbe420adbb819fdaffce481ac22289e4a1940cba
P S Shrijith <shrijithps89@gmail.com>
2023-11-30 08:04:34 +0530

    pong_game

--- Day21/Pong_Game/main.py
+++ Day21/Pong_Game/main.py
@@ -8,6 +8,15 @@ screen.bgcolor('white')
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
 paddle = Paddle()
+is_on = True
+screen.tracer(0)
+screen.listen()
 
+turtle.onkey(paddle.move_up, "Up")
+turtle.onkey(paddle.move_down, "Down")
+
+while is_on:
+    screen.update()
+    time.sleep(0.1)
 
 screen.exitonclick()
--- Day21/Pong_Game/paddle.py
+++ Day21/Pong_Game/paddle.py
@@ -1,13 +1,13 @@
 from turtle import Turtle
 
-positions = [(0, 20),(0, 40), (0, 60)]
+MOVE_SEGMENT = 40
+positions = [(350, 20), (350, 40), (350, 60), (350, 80)]
 
 turtle = Turtle()
 
 
 class Paddle:
     def __init__(self):
-        new_turtle = Turtle(shape='square')
         self.segments = []
         self.create_paddle()
 
@@ -17,7 +17,25 @@ class Paddle:
 
     def add_segment(self, position):
         new_turtle = Turtle(shape='square')
-        new_turtle.color('white')
         new_turtle.penup()
+        new_turtle.color('black')
         new_turtle.goto(position)
         self.segments.append(new_turtle)
+
+    def move_up(self):
+        self.segments[3].setheading(90)
+        self.segments[3].forward(MOVE_SEGMENT)
+        for turtle1 in range(len(self.segments) - 2, -1, -1):
+            new_xcor = self.segments[turtle1].xcor()
+            new_ycor = self.segments[turtle1].ycor() + MOVE_SEGMENT
+            self.segments[turtle1].goto(new_xcor, new_ycor)
+
+    def move_down(self):
+        self.segments[0].setheading(270)
+        self.segments[0].forward(MOVE_SEGMENT)
+        for turtle1 in range(len(self.segments)-1, 0, -1):
+            new_xcor = self.segments[turtle1].xcor()
+            new_ycor = self.segments[turtle1].ycor() - MOVE_SEGMENT
+            self.segments[turtle1].goto(new_xcor, new_ycor)
+
+
