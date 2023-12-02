6bdcb1c0e754c7e0f6aac4b493241c1e07d58007
P S Shrijith <shrijithps89@gmail.com>
2023-12-03 03:21:58 +0530

    pong_game

--- Day21/Pong Game/main.py
+++ Day21/Pong Game/main.py
@@ -1,6 +1,14 @@
+import turtle
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=800, height=600)
 
+paddle = Turtle()
+paddle.shape('square')
+paddle.color('black')
+paddle.shapesize(stretch_wid=9, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+screen.tracer()
 screen.exitonclick()
