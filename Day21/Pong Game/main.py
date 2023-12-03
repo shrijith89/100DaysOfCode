c51792b7fb32b5b03602a7bdb8358fd604e3d9a5
P S Shrijith <shrijithps89@gmail.com>
2023-12-03 16:39:19 +0530

    pong_game

--- Day21/Pong Game/main.py
+++ Day21/Pong Game/main.py
@@ -3,6 +3,7 @@ from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=800, height=600)
+is_on = True
 
 paddle = Turtle()
 paddle.shape('square')
@@ -11,4 +12,23 @@ paddle.shapesize(stretch_wid=9, stretch_len=1)
 paddle.penup()
 paddle.goto(350, 0)
 screen.tracer()
+
+
+def move_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def move_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.onkey(move_up, "Up")
+screen.onkey(move_down, "Down")
+
+screen.listen()
+
+
+
 screen.exitonclick()
