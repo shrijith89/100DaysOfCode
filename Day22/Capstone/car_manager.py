5b8555e1e10dff6a2f21bc49c86074d67b57bd35
P S Shrijith <shrijithps89@gmail.com>
2023-12-08 07:46:02 +0530

    capstone

--- Day22/Capstone/car_manager.py
+++ Day22/Capstone/car_manager.py
@@ -3,14 +3,14 @@ from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-STARTING_POSITION = (290, 0)
+
 
 
 class CarManager(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.goto(position)
         self.shape('square')
         self.color('red')
         self.shapesize(stretch_wid=1, stretch_len=2)
--- Day22/Capstone/main.py
+++ Day22/Capstone/main.py
@@ -4,12 +4,14 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 
+STARTING_POSITION = [(290, 0), (260, 20)]
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car_manager = CarManager()
+car_manager = CarManager(STARTING_POSITION[0])
+car2 = CarManager(STARTING_POSITION[1])
 
 turtle.onkey(player.move_forward, key='Up')
 screen.listen()
@@ -18,5 +20,6 @@ screen.listen()
 game_is_on = True
 while game_is_on:
     car_manager.move_car()
+    car2.move_car()
     time.sleep(0.1)
     screen.update()
