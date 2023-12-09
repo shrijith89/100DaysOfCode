fd97692609fc18cadd5b6607a19b2796dfbc7953
P S Shrijith <shrijithps89@gmail.com>
2023-12-09 22:21:52 +0530

    capstone

--- Day22/Capstone/car_manager.py
+++ Day22/Capstone/car_manager.py
@@ -1,19 +1,26 @@
 from turtle import Turtle
+import random
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
+STARTING_POSITION = [(290, 0), (260, 20), (210, 0)]
 
 
 class CarManager(Turtle):
-    def __init__(self, position):
-        super().__init__()
-        self.penup()
-        self.goto(position)
-        self.shape('square')
-        self.color('red')
-        self.shapesize(stretch_wid=1, stretch_len=2)
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        new_car = Turtle()
+        new_car.penup()
+        new_car.shape('square')
+        new_car.color(random.choice(COLORS))
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        y = random.randrange(-250, 250)
+        new_car.goto(280, y)
+        self.all_cars.append(new_car)
 
     def move_car(self):
-        self.backward(STARTING_MOVE_DISTANCE)
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
--- Day22/Capstone/main.py
+++ Day22/Capstone/main.py
@@ -4,22 +4,23 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 
-STARTING_POSITION = [(290, 0), (260, 20)]
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car_manager = CarManager(STARTING_POSITION[0])
-car2 = CarManager(STARTING_POSITION[1])
+car_manager = CarManager()
 
 turtle.onkey(player.move_forward, key='Up')
 screen.listen()
-
+car = CarManager()
 
 game_is_on = True
+flag = True
+
 while game_is_on:
-    car_manager.move_car()
-    car2.move_car()
-    time.sleep(0.1)
+    time.sleep(0.2)
     screen.update()
+    car.create_car()
+    car.move_car()
