8ebf884cd5b0a738ba21e8fb218169be1adbc6e2
P S Shrijith <shrijithps89@gmail.com>
2023-12-13 21:32:58 +0530

    snake_game

--- Day20/Snake Game/scoreboard.py
+++ Day20/Snake Game/scoreboard.py
@@ -13,13 +13,18 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = self.read_highscore()
         self.color('white')
         self.hideturtle()
         self.penup()
         self.goto(-20, 250)
         self.pendown()
         self.write(f"Score : {self.score} HighScore : {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    def read_highscore(self):
+        with open('data.txt') as file:
+            content = file.read()
+        return int(content)
 
     def update_scoreboard(self):
         self.clear()
