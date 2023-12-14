9acea93c33d4cae56ddc51fa296efc3d89375ed4
P S Shrijith <shrijithps89@gmail.com>
2023-12-14 10:23:52 +0530

    file_operations

--- Day23/Mail Merge Project Start/main.py
+++ Day23/Mail Merge Project Start/main.py
@@ -6,3 +6,18 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+names = []
+
+with open('./Input/Names/invited_names.txt') as f:
+    for line in f:
+        strip_line = line.strip()
+        names.append(strip_line)
+
+
+def write():
+    index = 0
+    with open('./Inout/Letters/starting_letter.txt') as f:
+        for line in f:
+            line.replace("[name]", names[index])
+
