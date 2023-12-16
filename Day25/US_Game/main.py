0be81ffae994a9afe5cc2657fc3f69d73a21809b
P S Shrijith <shrijithps89@gmail.com>
2023-12-16 22:28:31 +0530

    US_Game

--- Day25/US_Game/main.py
+++ Day25/US_Game/main.py
@@ -12,10 +12,16 @@ turtle.shape(image)
 while is_on:
     answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
     content = pandas.read_csv('50_states.csv')
-    valuex = content.loc[content.state == answer_state, 'x'].iloc[0]
-    valuey = content.loc[content.state == answer_state, 'y'].iloc[0]
-    turtle.penup()
-    turtle.goto(valuex, valuey)
-    turtle.write(answer_state)
+    if answer_state not in content['state'].tolist():
+        print("Not found")
+        is_on = False
+    else:
+        x_value = content.loc[content.state == answer_state, 'x'].iloc[0]
+        y_value = content.loc[content.state == answer_state, 'y'].iloc[0]
+        turtle.clear()
+        turtle.penup()
+        turtle.goto(x_value, y_value)
+        turtle.pendown()
+        turtle.write(answer_state)
 
 screen.exitonclick()
