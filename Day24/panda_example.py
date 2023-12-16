b986da57d19a07a343d71d64023f950d8eaba2cc
P S Shrijith <shrijithps89@gmail.com>
2023-12-16 17:08:05 +0530

    pandas

--- Day24/panda_example.py
+++ Day24/panda_example.py
@@ -7,3 +7,4 @@ print(sum(temperatures) / len(temperatures))
 
 print("The average temperature {}".format(content['temp'].mean()))
 print("The maximum temperature {}".format(content['temp'].max()))
+print("Details of {}".format(content[content.temp == content.temp.max()]))
