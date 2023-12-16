f9ae5e58cb83dce7a2817868abf60a1393dee9bc
P S Shrijith <shrijithps89@gmail.com>
2023-12-16 17:38:03 +0530

    pandas

--- Day24/panda_example.py
+++ Day24/panda_example.py
@@ -8,3 +8,13 @@ print(sum(temperatures) / len(temperatures))
 print("The average temperature {}".format(content['temp'].mean()))
 print("The maximum temperature {}".format(content['temp'].max()))
 print("Details of {}".format(content[content.temp == content.temp.max()]))
+
+#Create a dataframe
+
+data_dict = {
+    "students": ["ram", "shyam", "bheem"],
+    "scores": [67, 74, 77]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("mydataframe.csv")
