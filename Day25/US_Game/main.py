779fab9e60ff0659396e298b1e59b28c7798dc3e
P S Shrijith <shrijithps89@gmail.com>
2023-12-16 21:52:07 +0530

    US_Game

--- Day25/US_Game/main.py
+++ Day25/US_Game/main.py
@@ -1,4 +1,7 @@
 import turtle
+import pandas
+
+is_on = True
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -6,5 +9,13 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-screen.exitonclick()
+while is_on:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
+    content = pandas.read_csv('50_states.csv')
+    valuex = content.loc[content.state == answer_state, 'x'].iloc[0]
+    valuey = content.loc[content.state == answer_state, 'y'].iloc[0]
+    turtle.penup()
+    turtle.goto(valuex, valuey)
+    turtle.write(answer_state)
 
+screen.exitonclick()
