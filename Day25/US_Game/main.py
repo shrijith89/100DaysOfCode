c58d957b0c51615a01ae97248b58ca5663fd854f
P S Shrijith <shrijithps89@gmail.com>
2023-12-17 07:22:08 +0530

    US_Game

--- Day25/US_Game/main.py
+++ Day25/US_Game/main.py
@@ -2,20 +2,22 @@ import turtle
 import pandas
 
 is_on = True
-
+count = 0
 screen = turtle.Screen()
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-while is_on:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
-    content = pandas.read_csv('50_states.csv')
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
+content = pandas.read_csv('50_states.csv')
+length_of_content = len(content['state'].tolist())
+
+while count != length_of_content:
     if answer_state not in content['state'].tolist():
-        print("Not found")
-        is_on = False
+        pass
     else:
+        count += 1
         x_value = content.loc[content.state == answer_state, 'x'].iloc[0]
         y_value = content.loc[content.state == answer_state, 'y'].iloc[0]
         turtle.clear()
@@ -23,5 +25,8 @@ while is_on:
         turtle.goto(x_value, y_value)
         turtle.pendown()
         turtle.write(answer_state)
+    answer_state = screen.textinput(title="States correct ({}/{})".format(count, len(content['state'].tolist())), prompt="What's another state name?").title()
+
+print("Game complete")
 
 screen.exitonclick()
