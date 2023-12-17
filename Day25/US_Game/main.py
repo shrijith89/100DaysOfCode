9d45edc3a917886144f3ac645a29dc3843e111b3
P S Shrijith <shrijithps89@gmail.com>
2023-12-17 22:01:58 +0530

    US_Game

--- Day25/US_Game/main.py
+++ Day25/US_Game/main.py
@@ -1,5 +1,6 @@
 import turtle
 import pandas
+from pandas import DataFrame
 
 is_on = True
 count = 0
@@ -10,14 +11,17 @@ screen.addshape(image)
 turtle.shape(image)
 
 content = pandas.read_csv('50_states.csv')
+stateList = content['state'].tolist()
+entered_states = []
+missing_states = []
+
 
 while count != len(content['state'].tolist()):
     answer_state = screen.textinput(title="States correct ({}/{})".format(count, len(content['state'].tolist())),
                                     prompt="What's another state name?").title()
-    if answer_state not in content['state'].tolist():
-        pass
-    else:
+    if answer_state in content['state'].tolist():
         count += 1
+        entered_states.append(answer_state)
         x_value = content.loc[content.state == answer_state, 'x'].iloc[0]
         y_value = content.loc[content.state == answer_state, 'y'].iloc[0]
         t = turtle.Turtle()
@@ -27,6 +31,17 @@ while count != len(content['state'].tolist()):
         t.goto(x_value, y_value)
         t.pendown()
         t.write(answer_state)
+    elif answer_state == 'Exit':
+        for states in stateList:
+            if states not in entered_states:
+                missing_states.append(states)
+        break
 
-print("Game complete")
-screen.exitonclick()
+column_name = ["Missing_States"]
+
+df = pandas.DataFrame(missing_states, columns=column_name)
+csv_file_name = "missing_states.csv"
+
+df.to_csv(csv_file_name)
+
+
