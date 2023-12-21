8ea4e89a49a2bb97f87c10f95266057cc233b97e
P S Shrijith <shrijithps89@gmail.com>
2023-12-21 09:20:51 +0530

    dictionary_comprehension

---
+++ Day26/dictionary_comprehension4.py
@@ -0,0 +1,4 @@
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+
+weather_f = {i: j * (9/5)+32 for i, j in weather_c.items()}
+print(weather_f)
