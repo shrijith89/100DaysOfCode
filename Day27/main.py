f381c85ae52bca82e179642bdfd2e826ee8b1484
P S Shrijith <shrijithps89@gmail.com>
2023-12-25 18:21:59 +0530

    tkinter

--- Day27/main.py
+++ Day27/main.py
@@ -3,4 +3,8 @@ import tkinter
 window = tkinter.Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
 window.mainloop()
