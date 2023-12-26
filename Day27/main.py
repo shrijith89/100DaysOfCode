375cd80eb049b43c78693121833a6f54795aa9c2
P S Shrijith <shrijithps89@gmail.com>
2023-12-26 16:40:14 +0530

    positional_arguments

--- Day27/main.py
+++ Day27/main.py
@@ -5,6 +5,7 @@ window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.pack(side="top")
+
 
 window.mainloop()
