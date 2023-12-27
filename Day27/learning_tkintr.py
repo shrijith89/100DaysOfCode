37538e12d3937bb5a309d1235969a27febe614e5
P S Shrijith <shrijithps89@gmail.com>
2023-12-27 16:36:40 +0530

    tkinter_basics

--- Day27/learning_tkintr.py
+++ Day27/learning_tkintr.py
@@ -8,4 +8,27 @@ my_label = tkinter.Label(window, font=("Arial", 24, "bold"))
 
 my_label.config(text="New Text")
 my_label.pack()
+
+
+def button_clicked():
+    print("Button clicked")
+
+
+button = tkinter.Button(window, text="Click Me", command=button_clicked)
+button.pack()
+
+selected_value = tkinter.IntVar()
+
+radiobutton1 = tkinter.Radiobutton(text="option 1", variable=selected_value, value=1)
+radiobutton1.pack()
+
+radiobutton2 = tkinter.Radiobutton(text="option 2", variable=selected_value, value=2)
+radiobutton2.pack()
+
+radiobutton3 = tkinter.Radiobutton(text="option 3", variable=selected_value, value=3)
+radiobutton3.pack()
+
+radiobutton4 = tkinter.Radiobutton(text="option 4", variable=selected_value, value=4)
+radiobutton4.pack()
+
 window.mainloop()
