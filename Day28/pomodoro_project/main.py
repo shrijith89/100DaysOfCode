d4f79bdb78cf7d0135d855da724bc6cb60740092
P S Shrijith <shrijithps89@gmail.com>
2023-12-31 17:22:20 +0530

    pomodoro_project

--- Day28/pomodoro_project/main.py
+++ Day28/pomodoro_project/main.py
@@ -11,17 +11,32 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 2 == 0:
+        timer_label.config(text="Break", fg=GREEN)
+        count_down(short_break_sec)
+    elif reps % 8 == 0:
+        timer_label.config(text="Break Repetition 8")
+        count_down(long_break_sec)
+    else:
+        timer_label.config(text="Work")
+        count_down(work_sec)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 def count_down(count):
-
     count_min = math.floor(count / 60)
     count_seconds = (count % 60)
     if count_seconds == 0:
@@ -32,6 +47,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_seconds}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -53,12 +70,8 @@ timer_text = canvas.create_text(100, 140, text="00:00", font=(FONT_NAME, 35, "bo
 canvas.grid(row=1, column=2)
 
 
-def start_click():
-    count_down(SHORT_BREAK_MIN * 60)
-
-
 # startButton
-start_button = tkinter.Button(text="Start", command=start_click)
+start_button = tkinter.Button(text="Start", command=start_timer)
 start_button.grid(row=2, column=1, padx=5)
 
 # resetButton
