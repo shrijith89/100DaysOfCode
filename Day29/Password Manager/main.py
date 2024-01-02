2014a6f8fe184e67f55e569da5d95aaa0dfc0210
P S Shrijith <shrijithps89@gmail.com>
2024-01-02 09:56:09 +0530

    ui_setup

--- Day29/Password Manager/main.py
+++ Day29/Password Manager/main.py
@@ -1,5 +1,19 @@
+import tkinter
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = tkinter.Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+logo_png = tkinter.PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=logo_png)
+canvas.pack()
+
+window.mainloop()
