da6219653a691eaddc62474aa9f8fda781b213d1
P S Shrijith <shrijithps89@gmail.com>
2024-01-03 09:57:34 +0530

    ui_setup

--- Day29/Password Manager/main.py
+++ Day29/Password Manager/main.py
@@ -14,6 +14,27 @@ window.config(padx=50, pady=50)
 logo_png = tkinter.PhotoImage(file="logo.png")
 canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=logo_png)
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+websiteLabel = tkinter.Label(text="Website:")
+websiteLabel.grid(row=2, column=0)
+
+websiteInput = tkinter.Entry(width=35)
+websiteInput.grid(row=2, column=1, columnspan=2, padx=20)
+
+emailUsername = tkinter.Label(text="Email/Username:")
+emailUsername.grid(row=3, column=0, pady=5)
+
+emailInput = tkinter.Entry(width=35)
+emailInput.grid(row=3, column=1, columnspan=2, padx=20)
+
+passwordText = tkinter.Label(text="Password:")
+passwordText.grid(row=4, column=0, pady=5)
+
+passwordField = tkinter.Entry(width=21)
+passwordField.grid(row=4, column=1, padx=0)
+
+generateButton = tkinter.Button(text="Generate Password")
+generateButton.grid(row=4, column=2)
 
 window.mainloop()
