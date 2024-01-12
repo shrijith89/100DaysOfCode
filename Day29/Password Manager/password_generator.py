ff614d35dce33fced08ed0375a57a53d46d9baff
P S Shrijith <shrijithps89@gmail.com>
2024-01-12 23:31:45 +0530

    password_manager

--- Day29/Password Manager/password_generator.py
+++ Day29/Password Manager/password_generator.py
@@ -7,17 +7,6 @@ letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-letter_count = int(input("How many letters would you like in your password?"))
-symbol_count = int(input("How many symbols would you like in your password?"))
-number_count = int(input("How many numbers would you like in your password?"))
+nr_letters = random.randint(8, 10)
 
-password = []
-
-password += random.choices(letters, k=letter_count)
-password += random.choices(symbols, k=symbol_count)
-password += random.choices(numbers, k=number_count)
-
-random.shuffle(password)
-password_string = ''.join(password)
-print("Your password is : ".format(password_string))
+print(nr_letters)
