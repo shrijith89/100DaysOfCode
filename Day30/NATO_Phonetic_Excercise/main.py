68d1ed5ed1c62db45d8fd0a34e7195c1009c26de
P S Shrijith <shrijithps89@gmail.com>
2024-01-17 22:53:52 +0530

    nato_phoenetic_exercise

--- Day30/NATO_Phonetic_Excercise/main.py
+++ Day30/NATO_Phonetic_Excercise/main.py
@@ -4,12 +4,19 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+flag = True
+
+while flag:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+        flag = False
+    except KeyError:
+        print("Sorry, Only letters in the alphabet please.")
