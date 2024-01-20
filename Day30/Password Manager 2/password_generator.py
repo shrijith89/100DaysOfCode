6b98e0d0787ebbae6eeb15f15d5b47bbd35eca6b
P S Shrijith <shrijithps89@gmail.com>
2024-01-20 21:37:00 +0530

    password_manager

--- Day30/Password Manager 2/password_generator.py
+++ Day30/Password Manager 2/password_generator.py
@@ -1,6 +1,5 @@
 # Password Generator Project
 import random
-
 import pyperclip
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
